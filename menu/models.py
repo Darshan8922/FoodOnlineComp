9c13bfee4c9ec5adc25635167103aaafc9d24c01
Shah Darshan <darshanshah8922@gmail.com>
2024-04-01 18:12:01 +0530

    1st april

--- menu/models.py
+++ menu/models.py
@@ -7,12 +7,16 @@ class Category(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
     category_name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
+    description = models.TextField(max_length=250, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = 'category'
         verbose_name_plural = 'categories'
+
+    def clean(self):
+        self.category_name = self.category_name.capitalize()
 
     def __str__(self):
         return self.category_name
